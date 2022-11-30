3639ace22b4cb32182bb4335e94b3fdb82094932
Justin Wu <4genso@gmail.com>
2022-11-30 15:01:41 +0900

    style: deleted unnecessary comment

--- changer.js
+++ changer.js
@@ -2,7 +2,3 @@ var url_string = location.toString();
 var url = new URL(url_string);
 var img_format = url.searchParams.get("format");
 location.href = url_string.replace(/\?format=.*/, "."+img_format+":orig");
-
-
-
-// need to only repalce the orig part of the url and ignore anything after
